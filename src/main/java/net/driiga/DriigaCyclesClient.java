465ec3d552cc22d7912103132f36156ffbcf0d8d
Driiga <Chirazar7@gmail.com>
2025-11-29 02:00:33 +0300

    Initial commit

---
+++ src/main/java/net/driiga/DriigaCyclesClient.java
@@ -0,0 +1,10 @@
+package net.driiga;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DriigaCyclesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
